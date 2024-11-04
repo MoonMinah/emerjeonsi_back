da923b7886c4c657adf9150f6f7af81821743361
KOSA <mingke48@gmail.com>
2024-11-04 11:36:43 +0900

    empty

---
+++ src/main/java/com/kosa/emerjeonsi/EmerjeonsiApplication.java
@@ -0,0 +1,13 @@
+package com.kosa.emerjeonsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmerjeonsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmerjeonsiApplication.class, args);
+	}
+
+}
